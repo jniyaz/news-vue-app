2ea213dd88f4f07de565c1717a63356bc617c754
Niyaz <Niyaz>
2017-03-03 11:23:54 +0800

    first commit with all sources

---
+++ README.md
@@ -0,0 +1,29 @@
+# News App
+
+- Simple News app using VueJs and External newsapi.org api
+- This project has been included bootstrap, bootstrap material theme and jquery
+- Simple Vue Application, Much helpful learning resource in realtime.
+- Based on ideas of Mr Seeschweiler
+
+---
+Email me at [niyaz.www@gmail.com](Mailto:niyaz.www@gmail.com)
+
+<p align="center"><a href="https://niyazahamed.wordpress.com" target="_blank">Niyaz Tech. Blog</p>
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container" id="app">
+       <div class="alert alert-info alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        This is a simple news application using VueJs with external newsapi.org's api. Here you can load all the news from various popular news sources.
+      </div>
+      <div class="row">
+        <div class="col-md-8">
+          <div v-if="ads">
+            <p>Hello</p>
+          </div>
+          <Newslist v-bind:source="source"></Newslist>
+        </div>
+        <div class="col-md-4 pull-right">
+          <SourceSelection v-on:sourceChanged="sourceChanged"></SourceSelection>
+        </div>
+      </div>
+
+      <div class="footer">
+      <p class="credits">&copy <a href="http://niyazahamed.wordpress.com" target="_blank">More Tech. Updates</a> | <a href="https://www.youtube.com/channel/UC-NdJA98MuWwJF1a0YJCAeg" target="_blank">Channel</a> | Credits : Seeschweiler </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Newslist from './components/Newslist'
+import SourceSelection from './components/SourceSelection'
+
+export default {
+  name: 'app',
+  components: {
+    Newslist,
+    SourceSelection
+  },
+  data () {
+    return {
+      source: "",
+      ads: true
+    }
+  },
+  methods: {
+    sourceChanged: function (source) {
+      this.source = source;
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  padding-top: 20px;
+}
+.credits{font-size: 11px; color: #ccc;}
+</style>
